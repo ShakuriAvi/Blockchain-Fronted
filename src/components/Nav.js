fa855334eaa549fdec162db3b857a7e761cde649
ShakuriAvi <shakuri.avi96@gmail.com>
2021-07-30 11:21:50 +0300

    .

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,8 +2,13 @@ import React from "react";
 import style from "../style/App.css";
 import { Link } from "react-router-dom";
 import BlockchainImage from "../blockchain.png";
+import { useState } from "react";
 function Nav() {
+  const [active, setActive] = useState("hash");
 
+  const handleActive = (name) => {
+    setActive(name);
+  };
   return (
     <>
       <nav>
@@ -11,38 +16,65 @@ function Nav() {
           <img className="image" src={BlockchainImage} alt="" />
         </div>
         <ul className="nav-links">
-          <Link className={style.navStyle} to="/">
+          <Link
+            className={active === "hash" ? "nav-link active" : "nav-link"}
+            to="/"
+            onClick={() => handleActive("hash")}
+          >
             <li>
               <a>Hash</a>
             </li>
           </Link>
-          <Link className={style.navStyle} to="/Block">
-            <li><a>Block</a></li>
+          <Link
+            className={active === "block" ? "nav-link active" : "nav-link"}
+            to="/Block"
+            onClick={() => handleActive("block")}
+          >
+            <li>
+              <a>Block</a>
+            </li>
           </Link>
-          <Link className={style.navStyle} to="/Blockchain">
-            <li> <a> Blockchain</a></li>
+          <Link className={"nav-link"} to="/Blockchain">
+            <li>
+              {" "}
+              <a> Blockchain</a>
+            </li>
           </Link>
-          <Link className={style.navStyle} to="/Distributed">
-            <li><a> Distributed</a></li>
+          <Link className={"nav-link"} to="/Distributed">
+            <li>
+              <a> Distributed</a>
+            </li>
           </Link>
-          <Link className={style.navStyle} to="/Tokens">
-            <li><a>Tokens</a></li>
+          <Link className={"nav-link"} to="/Tokens">
+            <li>
+              <a>Tokens</a>
+            </li>
           </Link>
-          <Link className={style.navStyle} to="/CoinBase">
-            <li><a>CoinBase</a></li>
+          <Link className={"nav-link"} to="/CoinBase">
+            <li>
+              <a>CoinBase</a>
+            </li>
           </Link>
-          <Link className={style.navStyle} to="/Keys">
-            <li><a>Keys</a></li>
+          <Link className={"nav-link"} to="/Keys">
+            <li>
+              <a>Keys</a>
+            </li>
           </Link>
-          <Link className={style.navStyle} to="/Signatures">
-            <li><a>Signatures</a></li>
+          <Link className={"nav-link"} to="/Signatures">
+            <li>
+              <a>Signatures</a>
+            </li>
           </Link>
 
-          <Link className={style.navStyle} to="/Transaction">
-            <li><a>Transaction</a></li>
+          <Link className={"nav-link"} to="/Transaction">
+            <li>
+              <a>Transaction</a>
+            </li>
           </Link>
-          <Link className={style.navStyle} to="/BlockchainSigned">
-            <li><a>BlockchainSigned</a></li>
+          <Link className={"nav-link"} to="/BlockchainSigned">
+            <li>
+              <a>BlockchainSigned</a>
+            </li>
           </Link>
         </ul>
       </nav>
