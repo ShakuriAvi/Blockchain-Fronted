8b9c118b309c17d4ad902c888ec6d42181964c78
ShakuriAvi <shakuri.avi96@gmail.com>
2021-07-30 12:18:54 +0300

    .

--- src/components/Blockchain.js
+++ src/components/Blockchain.js
@@ -4,6 +4,10 @@ import React, { useEffect, useState } from "react";
 import Block from "./Block";
 import { css } from "@emotion/react";
 import axios from "axios";
+const fs = require("fs");
+const uri = fs.readFileSync("../dev.txt")
+  ? "http://localhost:3001"
+  : "https://blockchain-back-nodejs.herokuapp.com";
 
 const Blockchain = ({ type }) => {
   var send;
@@ -18,31 +22,30 @@ const Blockchain = ({ type }) => {
   `;
   const [loading, setLoading] = useState(true);
   const [changeBlock, setChangeBlock] = useState(true);
-const [counter, setCounter] = useState(1);
+  const [counter, setCounter] = useState(1);
   const [chain, setChain] = useState("");
   useEffect(() => {
-    if (send === "BlockChain"){
-      axios.get("http://localhost:3001/getblockchain").then((res) => {
-        setLoading(false);
-        setChain(res.data.chain);
-
-      });
-    }else if (send === "TX") {
-      axios.get("http://localhost:3001/gettokenblockchain").then((res) => {
-        setLoading(false);
-        setChain(res.data.chain);
-
-      });
-    }else if (send === "TX,CoinBase") {
-      axios.get("http://localhost:3001/getcoinbaseblockchain").then((res) => {
+    if (send === "BlockChain") {
+      axios.get(uri + "/getblockchain").then((res) => {
         setLoading(false);
         setChain(res.data.chain);
       });
-    }else{
-      axios.get("http://localhost:3001/getensignedblockchain").then((res) => {
+    } else if (send === "TX") {
+      axios.get(uri + "/gettokenblockchain").then((res) => {
         setLoading(false);
         setChain(res.data.chain);
-      }); }
+      });
+    } else if (send === "TX,CoinBase") {
+      axios.get(uri + "/getcoinbaseblockchain").then((res) => {
+        setLoading(false);
+        setChain(res.data.chain);
+      });
+    } else {
+      axios.get(uri + "/getensignedblockchain").then((res) => {
+        setLoading(false);
+        setChain(res.data.chain);
+      });
+    }
   }, []);
 
   const changePrev = (e) => {
@@ -51,16 +54,16 @@ const [counter, setCounter] = useState(1);
     //   setChain(chain)
     //  // console.log("chain" + JSON.stringify(chain));
     // }
-  
   };
-useEffect(()=>{
-console.log("chain" + JSON.stringify(chain));
-
-},[chain])
+  useEffect(() => {
+    console.log("chain" + JSON.stringify(chain));
+  }, [chain]);
   return (
     <>
+      {" "}
       {loading === false && chain !== "" && send === "BlockChain" ? (
         <div id="root" className={style.Row}>
+          {" "}
           {Object.values(chain).map((item, i) => (
             <Block
               key={i}
@@ -71,12 +74,13 @@ console.log("chain" + JSON.stringify(chain));
               initHash={item.hash}
               prop={changePrev}
             />
-          ))}
+          ))}{" "}
         </div>
       ) : loading === false &&
         chain !== "" &&
         (send === "TX" || send === "TX,CoinBase") ? (
         <div id="root" className={style.Row}>
+          {" "}
           {Object.values(chain).map((item, i) => (
             <Block
               key={i}
@@ -89,10 +93,11 @@ console.log("chain" + JSON.stringify(chain));
               initTx={item.data}
               initCoin={item.coinbase}
             />
-          ))}
+          ))}{" "}
         </div>
       ) : loading === false && chain !== "" && send === "TXKeys,CoinBase" ? (
         <div id="root" className={style.Row}>
+          {" "}
           {Object.values(chain).map((item, i) => (
             <Block
               key={i}
@@ -106,13 +111,13 @@ console.log("chain" + JSON.stringify(chain));
               initCoin={item.coinbase}
               // propTxKeys={changePrev}
             />
-          ))}
+          ))}{" "}
         </div>
       ) : (
         <div className={style.spinner}>
           <BeatLoader css={loaderCSS} size={72} color="green" loading />
         </div>
-      )}
+      )}{" "}
     </>
   );
 };
